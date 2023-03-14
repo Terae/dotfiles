df3a154f753c2316eb3f3b533da8546b424128d7
Benjamin BIGEY <benjamin.bigey@sigilence.com>
2023-03-14 11:52:18 +0100

    Renaming dotfiles inventory to local

--- Ansible/install.sh
+++ Ansible/install.sh
@@ -4,4 +4,4 @@ DIR_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR_ROOT}" || return 1
 
 ansible-galaxy collection install -r requirements.yml
-ansible-playbook -i hosts.ini dotfiles.yml --ask-become-pass
+ansible-playbook -i inventory/hosts.yml dotfiles.yml --ask-become-pass
