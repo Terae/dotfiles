d558688a24d5ac0a0d593d13e295e25871d4b5f3
Benjamin BIGEY <benjamin.bigey@sigilence.com>
2023-03-14 10:53:27 +0100

    Using Ansible Galaxy

--- Ansible/install.sh
+++ Ansible/install.sh
@@ -3,4 +3,5 @@
 DIR_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR_ROOT}" || return 1
 
-ansible-playbook -i hosts local_env.yml --ask-become-pass
+ansible-galaxy collection install -r requirements.yml
+ansible-playbook -i hosts.ini dotfiles.yml --ask-become-pass
